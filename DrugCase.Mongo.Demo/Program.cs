0fb04e36db7c678af7b4e3b6ef5eccb97ddcde99
aehyok <aehyok@vip.qq.com>
2016-10-21 23:17:20 +0800

    添加项目文件。

---
+++ DrugCase.Mongo.Demo/Program.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrugCase.Mongo.Demo
+{
+    public class user
+    {
+          public string Name { get; set; }
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var server = ConfigurationManager.AppSettings["MongoDbServer"].ToString();
+            var client = new MongoClient(server);
+            var database = client.GetDatabase("aehyok");
+            var sss = database.GetCollection<user>("user").Find(x=>x.Name=="aehyok");
+        }
+    }
+}
