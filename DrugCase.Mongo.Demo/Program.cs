dbf3081d594cdf3edb5e50fc4521a40e39e06e43
刘启明 <aehyok@vip.qq.com>
2016-10-22 11:54:29 +0800

    Customer 客户类 的MongoDb增删改查简单测试CustomerHelper 完成

--- DrugCase.Mongo.Demo/Program.cs
+++ DrugCase.Mongo.Demo/Program.cs
@@ -18,14 +18,53 @@ namespace DrugCase.Mongo.Demo
 
         static void Main(string[] args)
         {
+            //初始化配置文件
+            ConfigHelper.Init();
 
             try
             {
-                var server = ConfigurationManager.AppSettings["MongoDbServer"].ToString();
+                //#region 插入数据测试(注意数据量是100W条)
+                //int start=Environment.TickCount;
+                //for (int index = 0; index < 1000000; index++)
+                //{
+                //    Customer customer = new Customer()
+                //    {
+                //        Address = "深圳南山区",
+                //        ContactName = "0755-12345678",
+                //        CustomerName = "中科软" + (index + 1).ToString(),
+                //        PostalCode = "518000",
+                //        Telephone = "1388888888888"
+                //    };
+                //    CustomerHelper.Insert(customer);
+                //}
+                //int end = Environment.TickCount;
+                //Console.WriteLine("10000行数据处理耗时" + (end - start).ToString() + "ms");
+                //#endregion
 
-                var client = new MongoClient(server);
-                var database = client.GetDatabase("aehyok");
-                var sss = database.GetCollection<user>("user").Find(x => x.Name == "aehyok");
+                #region 更新
+                //Customer customer = new Customer()
+                //{
+                //    Address = "深圳南山区",
+                //    ContactName = "0755-12345678",
+                //    CustomerName = "腾讯科技",
+                //    PostalCode = "518000",
+                //    Telephone = "1389999999",
+                //    CustomerId = "c57e40ea343b4154ae3641e224525602"
+                //};
+
+                //CustomerHelper.Update(customer);
+                #endregion
+
+
+                #region 查询
+                ////var query = new QueryDocument { { "CustomerName", "中科软3" } };
+                //var query = new QueryDocument { { "CustomerId", new ObjectId("9cc11fa2aa7f400aba9a0649") } };
+                //var list = CustomerHelper.Find(query);
+                #endregion
+
+                CustomerHelper.Delete(new ObjectId("7f10ea7d8abf4c0285620f49"));
+                Console.ReadLine();
+
             }
             catch (Exception e)
             {
